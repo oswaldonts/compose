48caf97435cd060753d59eb7fd6c2ddbf530dce3
oswaldonts <atahualpaoswaldo@proton.me>
2024-05-19 15:11:34 +0000

    changes

--- front/app.js
+++ front/app.js
@@ -4,21 +4,28 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
-// Endpoint para obtener información del usuario
-app.get('/', async (req, res) => {
-  try {
-    // Realizar solicitud GET al endpoint /users/1
-    const response = await axios.get('http://localhost:8000/users/1'); // Cambia la URL según la dirección de tu servidor de backend
+app.get('/', (req, res) => {
+  const data = {
+    message: 'Hello World',
+  };
 
-    // Enviar la respuesta del servidor de backend como respuesta a la solicitud del cliente
+  res.json(data);
+});
+
+app.get('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const response = await axios.get(`http://localhost:8000/users/${userId}`);
+
     res.send(response.data);
   } catch (error) {
     console.error('Error al obtener información del usuario:', error);
+
     res.status(500).send('Error al obtener información del usuario');
   }
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor Express corriendo en el puerto ${PORT}`);
 });
